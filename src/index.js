b40b90b2da6b62d38c0a8768c6111a100505c0e2
zasara <raulzarza.dev@gmail.com>
2019-10-03 14:35:58 +0100

    version basica

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+
+
+
+//initializations
+const app = express();
+
+// Settings
+
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middlewares
+
+
+const storage = multer.diskStorage({
+    destination:path.join(__dirname, 'public/uploads'),
+    /* filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    } */
+});
+
+app.use(multer({
+    storage: storage,
+    dest: path.join(__dirname, 'public/uploads'),
+    limits: {fileSize: 100000},
+    fileFilter: (req, file, cb) => {
+        const filetypes = /jpeg|jpg|png|gif/
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname));
+        if (mimetype && extname){
+            return cb(null, true)
+        }
+        cb("Error: El archivo debe ser un archivo valido")
+    }
+}).single('image'));
+
+
+// Routes
+
+app.use(require('./routes/index'))
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')))
+// Start the server
+
+
+
+app.listen(app.get('port'),() => {
+ console.log(`Server on port ${app.get('port')}`);
+});
+
+
+
+
